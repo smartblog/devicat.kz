4d5a9bb50d1ef7ac859113e319c2b125a609acab
Boris Stukov <stukov.boris@gmail.com>
2017-05-31 20:45:55 +0600

    test for validations Question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates presence of title' do
+    expect(Question.new(body: 'Test text')).to_not be_valid
+  end
+
+  it 'validates presence of body' do
+    expect(Question.new(title: 'Title test')).to_not be_valid
+  end
 end
