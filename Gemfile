c600e4bf8a9709a66ee4635da325e7bf8d78161a
Boris Stukov <stukov.boris@gmail.com>
2017-05-31 21:05:00 +0600

    Add shoulda-matchers and refactoring test

--- Gemfile
+++ Gemfile
@@ -55,5 +55,9 @@ group :test, :development do
   gem 'factory_girl_rails'
 end
 
+group :test do
+  gem 'shoulda-matchers', git: 'https://github.com/thoughtbot/shoulda-matchers.git', branch: 'rails-5'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it 'validates presence of title' do
-    expect(Question.new(body: 'Test text')).to_not be_valid
-  end
-
-  it 'validates presence of body' do
-    expect(Question.new(title: 'Title test')).to_not be_valid
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 end
