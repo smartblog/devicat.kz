1c3864105a2f75889d843ccc5022e37d2b64713d
Boris Stukov <stukov.boris@gmail.com>
2017-06-08 20:50:20 +0600

    refactoring2 spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
-    it 'populates an array of all questions' do
-      questions = FactoryGirl.create_list(:question, 2)
+    let(:questions) { create_list(:question, 2) }
 
+    before do
       get :index
+    end
+
+    it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'renders index view' do
-      get :index
       expect(response).to render_template :index
     end
   end
