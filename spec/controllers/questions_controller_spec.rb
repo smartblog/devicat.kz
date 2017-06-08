a9fac5a2fc49d3ca124c888f4808bf80ecff7443
Boris Stukov <stukov.boris@gmail.com>
2017-06-08 20:29:54 +0600

    refactoring spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     it 'populates an array of all questions' do
-      question1 = FactoryGirl.create(:question)
-      question2 = FactoryGirl.create(:question)
+      questions = FactoryGirl.create_list(:question, 2)
 
       get :index
-      expect(assigns(:questions)).to match_array([question1, question2])
+      expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'renders index view' do
