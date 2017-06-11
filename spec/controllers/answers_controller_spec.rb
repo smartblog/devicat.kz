45f841f35b4aed207d26d212a21a90f574ca8e14
Boris Stukov <stukov.boris@gmail.com>
2017-06-11 14:36:06 +0600

    Add specs and factories to answers_controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question) }
 
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    before { question }
+
+    context 'with valid params' do
+      it 'save new answer in database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+      it 'redirect to show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to answer_path(assigns(:answer))
+      end
+    end
+
+    context 'with invalid params' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) } }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
