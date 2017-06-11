551cb0be30e658987ab9a5e417ea70b42c27858d
Boris Stukov <stukov.boris@gmail.com>
2017-06-11 14:44:58 +0600

    Add method new in answers_controller and views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,11 @@
 class AnswersController < ApplicationController
+  before_action :set_question, only: [:new]
+
+  def new
+    @answer = Answer.new
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 end
