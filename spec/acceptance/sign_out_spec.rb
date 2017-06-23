78978c51f9f97b63c9f5221731d15b3fc909dc96
Boris Stukov <stukov.boris@gmail.com>
2017-06-23 17:16:37 +0600

    Add acceptance test to sign_out

--- spec/acceptance/sign_out_spec.rb
+++ spec/acceptance/sign_out_spec.rb
@@ -4,6 +4,16 @@ feature 'User sign out', %q{
   To finish session
   user need to sign out
 } do
-  scenario 'Authenticated user try to sign out'
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user try to sign out' do
+    sign_in(user)
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully'
+    expect(page).to have_content 'Sign in'
+  end
+
   scenario 'Unauthenticated user try to sign out'
 end
