d569670ee3e31b1e4a138296db55408ca4ef9bd7
Boris Stukov <stukov.boris@gmail.com>
2017-06-23 18:52:45 +0600

    Update spec delete_questions

--- spec/acceptance/delete_question_spec.rb
+++ spec/acceptance/delete_question_spec.rb
@@ -5,6 +5,20 @@ feature 'User delete his questions', %q{
   but can't delete not his questions
 } do
 
-  scenario 'User try delete his question'
-  scenario 'User try delete not his question'
+  scenario 'User try delete his question' do
+    sign_in(user)
+
+    visit question_path(user.questions.first)
+    click_on 'Delete question'
+
+    expect(page).to have_content 'Your question successfully deleted'
+  end
+
+  scenario 'User try delete not his question' do
+    sign_in(user)
+
+    visit question_path(user2.questions.first)
+    
+    expect(page).to have_no_content 'Delete question'
+  end
 end
