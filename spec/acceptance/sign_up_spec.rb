ebf518f2425a2b5d405096cd7c095fc6fc508d1d
Boris Stukov <stukov.boris@gmail.com>
2017-06-23 18:29:09 +0600

    Add spec sign_up scenarios

---
+++ spec/acceptance/sign_up_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'User Sign-Up', %q{
+  To Login user
+  need to sign_up on site
+} do
+
+  scenario 'Non-registered user tries to registrate'
+  scenario 'Authenticated user tries to registrate'
+end
