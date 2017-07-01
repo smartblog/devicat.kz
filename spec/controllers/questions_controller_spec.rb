a411351a81741504fd23b145fa8c8fcf8f08581a
Boris Stukov <stukov.boris@gmail.com>
2017-07-02 00:38:58 +0600

    Add edit questions AJAX tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -87,24 +87,24 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'PATCH #update' do
     sign_in_user
-    let(:users_question) { create(:question, user: @user) }
+    let!(:users_question) { create(:question, user: @user) }
 
     context 'valid attributes' do
       it 'assigns the requested question to @question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
+        patch :update, params: { id: question, question: attributes_for(:question) }, format: :js
         expect(assigns(:question)).to eq question
       end
 
       it 'change question attributes' do
-        patch :update, params: { id: users_question, question: { title: 'new title', body: 'new body' } }
+        patch :update, params: { id: users_question, question: { title: 'new title', body: 'new body' } }, format: :js
         users_question.reload
         expect(users_question.title).to eq 'new title'
         expect(users_question.body).to eq 'new body'
       end
 
-      it 'redirects to the updated question' do
-        patch :update, params: { id: users_question, question: attributes_for(:question) }
-        expect(response).to redirect_to users_question
+      it 'render updated question' do
+        patch :update, params: { id: users_question, question: attributes_for(:question) }, format: :js
+        expect(response).to render_template :update
       end
     end
 
@@ -112,7 +112,7 @@ RSpec.describe QuestionsController, type: :controller do
       let(:title) { users_question.title }
       let(:body) { users_question.body }
 
-      before { patch :update, params: { id: users_question, question: { title: 'new title', body: nil } } }
+      before { patch :update, params: { id: users_question, question: { title: 'new title', body: nil } }, format: :js }
 
       it 'does not change question attributes' do
         users_question.reload
@@ -121,7 +121,7 @@ RSpec.describe QuestionsController, type: :controller do
       end
 
       it 're-renders edit view' do
-        expect(response).to render_template :edit
+        expect(response).to render_template :update
       end
     end
   end
