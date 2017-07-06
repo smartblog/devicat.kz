bd6e6b7141f09c3ac9e2614b7707c1ae14d6c787
Boris Stukov <stukov.boris@gmail.com>
2017-07-06 09:33:39 +0600

    Delete attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,2 +1,6 @@
 class AttachmentsController < ApplicationController
+  def destroy
+    @attachment = Attachment.find(params[:id])
+    @attachment.destroy if current_user.author?(@attachment.attachmentable)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :attachments, only: [:destroy]
+
   root to: "questions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
