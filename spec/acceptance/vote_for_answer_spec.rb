8093371d02b0543bf4eddc230f5387f8c92f15a0
Boris Stukov <stukov.boris@gmail.com>
2017-07-07 20:41:09 +0600

    update vote answer specs

--- spec/acceptance/vote_for_answer_spec.rb
+++ spec/acceptance/vote_for_answer_spec.rb
@@ -5,7 +5,53 @@ feature 'Vote for answer', %q{
   Authenticated user
   can vote for answer
 } do
-  scenario 'authenticated user vote for answer'
-  scenario 'author try vote for his answer'
-  scenario 'non-authenticated user try to vote for answer'
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+
+  scenario 'authenticated user like answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.answers' do
+      choose 'Like'
+      click_on 'Vote'
+    end
+  end
+
+  scenario 'authenticated user dislike answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.answers' do
+      choose 'Dislike'
+      click_on 'Vote'
+    end
+  end
+
+  scenario 'authenticated user try vote answer once more time', js: true do
+    sign_in(user)
+    visit question_path(question)
+    within '.answers' do
+      choose 'Like'
+      click_on 'Vote'
+      expect(page).to have_no_button 'Vote'
+    end
+  end
+
+  scenario 'author try vote for his answer' do
+    sign_in(answer.user)
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to have_no_button 'Vote'
+    end
+  end
+
+  scenario 'non-authenticated user try vote for answer' do
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to have_no_button 'Vote'
+    end
+  end
 end
